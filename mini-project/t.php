e316d0fce56baf5bbb285ad12f5b414997dbc622
varuniherle <47895542+varuniherle@users.noreply.github.com>
2021-01-02 13:27:43 +0530

    Delete t.php

--- mini-project/t.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'database.php';
-$query = "select max(question_number) as max_no from questions";
-$questions = $mysqli->query($query) or die ($mysqli->error.__LINE__);
-$result = $questions->fetch_assoc();
-echo $result['max_no']+1;
-?>

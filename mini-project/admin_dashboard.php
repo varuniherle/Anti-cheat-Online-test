410dd5a7c6611f7eaa5224bbcd3f191ae161b6ab
Dhruvita Banugaria <47861010+dhruvitahrishi@users.noreply.github.com>
2021-01-04 21:52:44 +0530

    Update admin_dashboard.php

--- mini-project/admin_dashboard.php
+++ mini-project/admin_dashboard.php
@@ -1,29 +1,54 @@
-<?php
-session_start();
-#echo $_SESSION['user'];
- ?>
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Dashboard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
-    <link rel="stylesheet" type="text/css" href="css/common.css">
-</head>
-<body>
-<div class="sidebar">
-  <a class="active" href="admin_dashboard.php"> Home </a>
-  <a href="add_exam.php">Add Skill Test</a>
-  <a href="delete_exam.php">Delete Exam</a>
-  
-</div>
 
-<div class="content">
-  <h1 class = "main">Hidden Answers</h1>
-  <br><br>
-  
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HA-Admin-Dashboard</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="common.css">
+    <link rel="stylesheet" href="features.css" type="text/css">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div class="content">
+        <h1 class="main" style="text-align: center;">Hidden Answers Admin</h1>
+    </div>
+    <nav class="navbar navbar-expand-sm  navbar-light sticky-top" style="background-color: #e3f2fd;">
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_dashboard.php">Admin dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add_exam.php">Add Exam</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="delete_exam.php">Delete Exam</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+
 
 </body>
+
 </html>
